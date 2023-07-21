4ec7403b8e0b45585b464845a189333f6e02b58c
pc <pc@DESKTOP-BNF6OT5.local>
2023-07-21 09:12:05 +0300

    method örnekleri çözüldü.

--- src/com/bilgeadam/week3/lecture003/Question40.java
+++ src/com/bilgeadam/week3/lecture003/Question40.java
@@ -1,5 +1,7 @@
 package com.bilgeadam.week3.lecture003;
 
+import java.util.Arrays;
+
 public class Question40 {
 
 	public static void main(String[] args) {
@@ -31,20 +33,31 @@ public class Question40 {
 
 		int start = 2;
 		int end = 22;
-		String sayilar[] = new String[(end - start) + 2];
+		String sayilar[] = new String[(end - start) + 1];
 
 		for (int i = 0; i < sayilar.length; i++) {
-			if ((i + 2) % 3 == 0 && (i + 2) % 5 == 0) {
-				sayilar[i] = "bileadam";
-				System.out.println(i + ". index =" + (i + 2) + " -> " + sayilar[i]);
-			} else if ((i + 2) % 3 == 0) {
-				sayilar[i] = "bilge";
-				System.out.println(i + ". index =" + (i + 2) + " -> " + sayilar[i]);
-			} else if ((i + 2) % 5 == 0) {
-				sayilar[i] = "adam";
-				System.out.println(i + ". index =" + (i + 2) + " -> " + sayilar[i]);
-			}
+			if ((i + start) % 15 == 0) { // ben hem 3e hem 5 e bölüm diye bakmıştım bu daha ksıa
+				sayilar[i] = "Bileadam";
+
+			} else if ((i + start) % 3 == 0) {
+				sayilar[i] = "Bilge";
+
+			} else if ((i + start) % 5 == 0) {
+				sayilar[i] = "Adam";
+
+			} else
+
+				sayilar[i] = "" + (i + start);
 		}
+
+//		for (int i = 0; i < sayilar.length; i++) {
+//			if (i == sayilar.length - 1) {
+//				System.out.println(sayilar[i]);
+//			} else {
+//				System.out.print(sayilar[i] + " ,");
+//			}
+//		}
+		System.out.println(Arrays.toString(sayilar)); // -> array yazdırma methodu
 
 	}
 
